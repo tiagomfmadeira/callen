f48a0afb8a858ed817db3a475306a6760b04ba15
Tiago Madeira <tiagomfmadeira@gmail.com>
2020-11-15 23:49:51 +0000

    printing working

--- Callen/Callen/Windows/winPrint.xaml.cs
+++ Callen/Callen/Windows/winPrint.xaml.cs
@@ -58,26 +58,49 @@ namespace Callen.Windows
 
             List<Instance> instances_to_print = (App.Current.Properties["PrintList"] as List<Instance>);
 
-            for (int i = 0; i < instances_to_print.Count; i = i + 2)
+            int nr_of_ws = instances_to_print.Count / 36;
+            if(instances_to_print.Count % 36 > 0)
             {
-                ws.Range["A" + (i + 1)].Value = "Nº" + instances_to_print[i].inst_num;
-                ws.Range["B" + (i + 1)].Value = instances_to_print[i].other;
-                ws.Range["C" + (i + 1)].Value = instances_to_print[i].collec;
-                ws.Range["D" + (i + 1)].Value = instances_to_print[i].year;
-                ws.Range["A" + (i + 2)].Value = instances_to_print[i].name;
-
-                if (i + 1 < instances_to_print.Count)
-                {
-                    ws.Range["E" + (i + 1)].Value = "Nº" + instances_to_print[i + 1].inst_num;
-                    ws.Range["F" + (i + 1)].Value = instances_to_print[i + 1].other;
-                    ws.Range["G" + (i + 1)].Value = instances_to_print[i + 1].collec;
-                    ws.Range["H" + (i + 1)].Value = instances_to_print[i + 1].year;
-                    ws.Range["E" + (i + 2)].Value = instances_to_print[i + 1].name;
-                }
+                nr_of_ws++;
             }
 
-            ws.Copy(Type.Missing, wb.Worksheets[1]);
+            for (int i = 1; i < nr_of_ws; i++)
+            {
+                ws.Copy(Type.Missing, wb.Worksheets[wb.Sheets.Count]);
+                wb.Sheets[wb.Sheets.Count].Name = "Folha" + i;
+            }
 
+            int k = 0;
+            for (int j = 1; j <= nr_of_ws; j++)
+            {
+                ws = wb.Worksheets[j];
+                for (int i = 0; i < 36; i = i + 2)
+                {
+                    if (k >= instances_to_print.Count)
+                    {
+                        break;
+                    }
+
+                    ws.Range["A" + (i + 1)].Value = "Nº" + instances_to_print[k].inst_num;
+                    ws.Range["B" + (i + 1)].Value = instances_to_print[k].other;
+                    ws.Range["C" + (i + 1)].Value = instances_to_print[k].collec;
+                    ws.Range["D" + (i + 1)].Value = instances_to_print[k].year;
+                    ws.Range["A" + (i + 2)].Value = instances_to_print[k].name;
+                    k++;
+
+                    if (k >= instances_to_print.Count)
+                    {
+                        break;
+                    }
+
+                    ws.Range["E" + (i + 1)].Value = "Nº" + instances_to_print[k].inst_num;
+                    ws.Range["F" + (i + 1)].Value = instances_to_print[k].other;
+                    ws.Range["G" + (i + 1)].Value = instances_to_print[k].collec;
+                    ws.Range["H" + (i + 1)].Value = instances_to_print[k].year;
+                    ws.Range["E" + (i + 2)].Value = instances_to_print[k].name;
+                    k++;
+                }
+            }
             wb.SaveAs("C:\\Users\\NegativeSpade\\Desktop\\Etiquetas_para_imprimir_"+ DateTime.Now.ToString("yyyy-M-dd_HH-mm-ss") + ".xlsx");
         }
 
